7feb83798d446ab3602f77e660d3c19d5d62d9a6
Fabian Weisshaar <elnappo@nerdpol.io>
2019-01-23 15:00:50 +0100

    Add molecule for testing

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,16 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_check_mk_agent_server_is_installed(host):
+    check_mk_agent = host.package('check-mk-agent')
+
+    assert check_mk_agent.is_installed
+
+
+def test_check_mk_agent_socket_is_listening(host):
+    assert host.socket("tcp://0.0.0.0:6656").is_listening
